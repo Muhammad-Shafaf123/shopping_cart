3d144e1aace9d3ad925d2d190fcdede41f617ec9
Muhammed <shafafmuhammad6@gmail.com>
2021-05-28 05:31:18 +0530

    admin config done

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(ProductMen)
+admin.site.register(ProductWomen)
+admin.site.register(ProductKids)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
+
+
+
--- store/views.py
+++ store/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def store(request):
-    context={}
-    return render(request, 'store/store.html')
+    products = ProductMen.objects.all()
+    productsWomen = ProductWomen.objects.all()
+    productsKids = ProductKids.objects.all()
+
+    context={'products':products, 'productsWomen':productsWomen, 'productsKids':productsKids}
+    return render(request, 'store/store.html', context)
 
 def cart(request):
     context={}
